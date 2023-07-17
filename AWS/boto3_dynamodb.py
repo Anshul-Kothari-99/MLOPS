3a765cc28a2d5d1d179bbe702d3530110f11a8ad
Anshul Kothari <67574233+Anshul-Kothari-99@users.noreply.github.com>
2023-07-17 09:30:21 +0530

    AWS Credentials Deleted

--- AWS/boto3_dynamodb.py
+++ AWS/boto3_dynamodb.py
@@ -12,8 +12,8 @@ import os
 from time import sleep
 
 # Credentials for Authorization
-access_id='AKIA2WV3P6RXZXT2KVWI'
-secret_access_key='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'
+access_id='XXXXXX'
+secret_access_key='XXXXXX'
 
 folder_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
 resource_to_use = 'dynamodb'
